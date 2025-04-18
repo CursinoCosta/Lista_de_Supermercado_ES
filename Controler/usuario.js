18bc9f0e919d3956196da9034cc400b80973ef77
CursinoCosta] <mateuscgcosta@gmail.com>
2025-04-18 17:35:37 -0300

    commit pausa avanço db usuario e outras tabelas criadas

--- Controler/usuario.js
+++ Controler/usuario.js
@@ -2,7 +2,14 @@ import { openDb } from "../src/configDb.js";
 
 export async function createTable(){
     openDb().then(db => {
-        db.exec('CREATE TABLE IF NOT EXISTS Usuario (id INTEGER PRIMARY KEY, email TEXT, nomeCompleto TEXT, nomeUsuario TEXT, senha TEXT)')
+        db.exec('CREATE TABLE IF NOT EXISTS Usuarios (UsuarioID INTEGER PRIMARY KEY, email TEXT, nomeCompleto TEXT, nomeUsuario TEXT, senha TEXT)')
+        
+        db.exec('CREATE TABLE IF NOT EXISTS Receitas (ReceitaID INTEGER PRIMARY KEY, UsuarioID INTEGER,	NomeReceita TEXT, Categoria TEXT, Favorito BINARY, FOREIGN KEY (UsuarioID) REFERENCES Usuario(UsuarioID) )')
+        db.exec('CREATE TABLE IF NOT EXISTS IngredientesReceitas (ReceitaID INTEGER, Ingrediente TEXT,	NomeReceita TEXT, Quantidade INTEGER, UnidadeMedida TEXT, FOREIGN KEY (ReceitaID) REFERENCES Receitas(ReceitaID) )')
+        db.exec('CREATE TABLE IF NOT EXISTS InstrucaoReceitas (ReceitaID INTEGER, Instrucao TEXT, FOREIGN KEY (ReceitaID) REFERENCES Receitas(ReceitaID) )')
+        
+        db.exec('CREATE TABLE IF NOT EXISTS Lista (Item TEXT, Quantidade INTEGER, UnidadeMedida TEXT, Prioridade INTEGER )')
+
     })
 }
 
@@ -10,4 +17,16 @@ export async function insertUsuario(User){
     openDb().then(db => {
         db.run('INSERT INTO Usuario (email, nomeCompleto, nomeUsuario, senha) VALUES (?, ?, ?, ?)',[User.email, User.nomeCompleto, User.nomeUsuario, User.senha])
     })
+}
+
+export async function updateUsuario(id, User) {
+    openDb().then(db => {
+        db.run('UPDATE Usuario SET email=?, nomeCompleto=?, nomeUsuario=?, senha=? WHERE id=?', [User.email, User.nomeCompleto, User.nomeUsuario, User.senha, id]);
+    });
+}
+
+export async function deleteUsuario(id) {
+    openDb().then(db => {
+        db.run('DELETE FROM Usuario WHERE id=?', [id]);
+    });
 }
